475699a723c39daead1c67db0c209663497e73ae
Andy Scott <andy.g.scott@gmail.com>
2016-08-18 16:50:14 -0700

    Nix unused predef related methods

--- src/reflect/scala/reflect/internal/TreeInfo.scala
+++ src/reflect/scala/reflect/internal/TreeInfo.scala
@@ -850,25 +850,6 @@ abstract class TreeInfo {
     }
   }
 
-  /** Is this file the body of a compilation unit which should not
-   *  have Predef imported?
-   */
-  def noPredefImportForUnit(body: Tree) = {
-    // Top-level definition whose leading imports include Predef.
-    def isLeadingPredefImport(defn: Tree): Boolean = defn match {
-      case PackageDef(_, defs1) => defs1 exists isLeadingPredefImport
-      case Import(expr, _)      => isReferenceToPredef(expr)
-      case _                    => false
-    }
-    // Compilation unit is class or object 'name' in package 'scala'
-    def isUnitInScala(tree: Tree, name: Name) = tree match {
-      case PackageDef(Ident(nme.scala_), defs) => firstDefinesClassOrObject(defs, name)
-      case _                                   => false
-    }
-
-    isUnitInScala(body, nme.Predef) || isLeadingPredefImport(body)
-  }
-
   def isAbsTypeDef(tree: Tree) = tree match {
     case TypeDef(_, _, _, TypeBoundsTree(_, _)) => true
     case TypeDef(_, _, _, rhs) => rhs.tpe.isInstanceOf[TypeBounds]
